9b88a85b7472be54c9ec9cecd5fdc62956fd237e
susmitha8199 <selluru@evoketechnologies.com>
2025-10-06 12:00:16 +0530

    activate route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from 'src/components/home-page/home-page.component';
 import { LoginPageComponent } from 'src/components/login-page/login-page.component';
 import { DashboardComponent } from 'src/components/dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
